aa1b460ee7e8a8b6459a7cab7bef0e2a1c3be440
nishankafle36-ctrl <nishankafle36@gmail.com>
2026-04-17 23:07:28 -0400

    Day 4: Learning conditionals

---
+++ choice.py
@@ -0,0 +1,10 @@
+company = input("Which company are you applying to? ").lower()
+
+if company == "google":
+    print("Focus on Data Structures and the STEP internships!")
+elif company == "amazon":
+    print("Memorize those 16 Leadership Principles!")
+elif company == "microsoft":
+    print("Research the Microsoft Explore program!")
+else:
+    print("Keep grinding and build the portfolio!")
