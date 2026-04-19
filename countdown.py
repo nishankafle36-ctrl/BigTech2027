810435f1e49b0d3c227bfd093ece96e9e6f43050
nishankafle36-ctrl <nishankafle36@gmail.com>
2026-04-18 22:49:18 -0400

    Day 5: Learning Loops and countdowns

---
+++ countdown.py
@@ -0,0 +1,7 @@
+import time 
+
+# Counting down 5 nights of study
+for i in [5, 4, 3, 2, 1]:
+    print(f"Only {i} nights of study left until your next big milestone!")
+    time.sleep(1)  #This pauses the computer for 1 second 
+print("Morgan Tech Fest is coming! Are you ready")
